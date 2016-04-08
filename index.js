064e8037d7fce67e100283d707c3a2049f66816e
yuwancumian <yuwancumian@hotmail.com>
2016-04-08 16:53:40 +0800

    basic features

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+require('shelljs/global');
+var path = require('path');
+var cfg = require('./config');
+var argv = require('yargs')
+           .alias('o','output')             
+           .argv;
+
+(function(){
+
+  var project = process.argv[2]
+	var src = cfg[project].src;
+	var dest = cfg[project].dest;
+	console.log("src:" + src);
+	console.log("dest:" + dest);
+	exec('rsync -avP ' + src +" " + dest);
+
+})();
+
