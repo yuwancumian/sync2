cfba21a0f6130db97959338b10b6fa06d98bc620
yuwancumian <yuwancumian@hotmail.com>
2016-04-11 17:22:18 +0800

    add no argvs

--- index.js
+++ index.js
@@ -4,28 +4,34 @@ var path = require('path');
 var chalk = require('chalk');
 var cfg = require('./config');
 
-
 (function(){
 
   var project = process.argv[2]
-	var src = cfg[project].src;
-	var dest = cfg[project].dest;
-	function getUserHome() {
-  	return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+
+  if (!process.argv[2] || process.argv[2] === "") {
+        console.log(chalk.red('Sorry, a project name must be assigned!'));
+        return;
+  } else {
+
+		var src = cfg[project].src;
+		var dest = cfg[project].dest;
+		function getUserHome() {
+	  	return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+		}
+
+		if (src === "github") {
+			//cd(getUserHome());
+			console.log(path.join(userHome(),cfg[project].dest));
+			//console.log(cfg[project].dest);
+			//exec('git pull origin master');
+		} else {
+			exec('rsync -avP ' + src +" " + dest);
+		}
+		console.log("");
+		console.log(chalk.green(project +" has been synced!"))
+		console.log("");
+		console.log("Sync files from "+ chalk.magenta(src) + " to " + chalk.magenta(dest));
 	}
-	if (src === "github") {
-		cd(getUserHome());
-		cd('Library/Application Support/Sublime Text 3/Packages/User')
-		console.log("###");
-		console.log(pwd());
-		exec('git pull origin master');
-	} else {
-		exec('rsync -avP ' + src +" " + dest);
-	}
-	console.log("");
-	console.log(chalk.green(project +" has been synced!"))
-	console.log("");
-	console.log("Sync files from "+ chalk.magenta(src) + " to " + chalk.magenta(dest));
 	
 })();
 
